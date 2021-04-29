f7a3aaf049809d566fc4b70569efc83684fc0943
mGilbert15 <mgilbert@consultwithcase.com>
2021-04-29 12:27:03 -0400

    create-new-spring-boot-project: created spring project with initializr

---
+++ src/main/java/guru/springframework/slgdi/SlgDiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.slgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlgDiApplication.class, args);
+	}
+
+}
