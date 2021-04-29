0b00abbe97e9ddcc3d2ef73342a07f9586b2b2e1
mGilbert15 <mgilbert@consultwithcase.com>
2021-04-29 12:44:38 -0400

    create-a-controller-and-access-bean-in-application: create a controller and access the controller using bean in application

--- src/main/java/guru/springframework/slgdi/SlgDiApplication.java
+++ src/main/java/guru/springframework/slgdi/SlgDiApplication.java
@@ -2,12 +2,21 @@ package guru.springframework.slgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.slgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SlgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SlgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SlgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
